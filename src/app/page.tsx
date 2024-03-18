50ec8a37c67eee2ad3583701239fe09637156c7a
ybenali <youssef.benali@digimind.com>
2024-03-18 11:37:05 +0100

    feat: fetch and display product widgets data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ProductWidgetList from '@/components/productWidgetList';
+
 export type ProductData = {
   readonly id: number;
   readonly amount: number;
@@ -8,6 +10,34 @@ export type ProductData = {
   readonly action: 'collects' | 'plants' | 'offsets';
 };
 
-export default function Home() {
-  return <main>greenspark-task</main>;
-}
+const getWidgetsData = async (): Promise<ProductData[]> => {
+  const res = await fetch(`${process.env.API_ENDPOINT_URL}/product-widgets`);
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+};
+
+const Page = async () => {
+  const data = await getWidgetsData();
+
+  return (
+    <main className="flex w-full h-full items-center justify-center p-8">
+      <div className="shadow-2xl p-9 rounded-lg w-[851px] min-h-[419px] max-w-[90vw]">
+        <h1 className="font-[700] text-[30px] leading-[36px] text-center lg:text-start pb-1">
+          Per product widgets
+        </h1>
+        <hr className="mt-2 mb-4 border-2 border-solid border-silver" />
+        <ProductWidgetList
+          className="flex justify-between items-center max-lg:flex-col max-lg:gap-y-9 p-1"
+          data={data}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Page;
