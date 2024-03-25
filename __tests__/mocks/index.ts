7a37241fb4a66ca0a7730d4869fd748e38e5bb26
ybenali <youssef.benali@digimind.com>
2024-03-25 21:15:30 +0100

    chore(deps): setup Jest and testing-library/react

---
+++ __tests__/mocks/index.ts
@@ -0,0 +1,31 @@
+import { ProductData } from '@/app/page';
+
+export const productWidgetListData: ProductData[] = [
+  {
+    id: 1,
+    type: 'plastic bottles',
+    amount: 100,
+    action: 'collects',
+    active: true,
+    linked: true,
+    selectedColor: 'green',
+  },
+  {
+    id: 2,
+    type: 'trees',
+    amount: 10,
+    action: 'plants',
+    active: false,
+    linked: false,
+    selectedColor: 'black',
+  },
+  {
+    id: 3,
+    type: 'carbon',
+    amount: 20,
+    action: 'offsets',
+    active: false,
+    linked: false,
+    selectedColor: 'blue',
+  },
+];
